8a6fb540a019b8da777c2e0902e9ac5443a31b6a
Karol Warakomski <k.warakomski1@gmail.com>
2018-08-05 20:46:25 +0200

    add UserData class

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String userName[] = { "Karol", "Wojtek", "Dorota", "Marian", "Krzysiek", "Jan" };
+
+        for(int i=0; i<userName.length; i++){
+
+        }
+
+
+    }
+}
---
+++ src/registration/UserData.java
@@ -0,0 +1,21 @@
+package registration;
+
+public class UserData {
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public UserData(String userName, String firstName, String lastName, String email, String password) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+
+
+}
