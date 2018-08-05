85d3fa175717760db007c93a2fefbccf39b27b0a
Wojciech Abram <wojciech.m.abram@gmail.com>
2018-08-05 21:04:20 +0200

    Random Name for the

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
         String userName[] = { "Karol", "Wojtek", "Dorota", "Marian", "Krzysiek", "Jan" };
 
-        for(int i=0; i<userName.length; i++){
+        String randomElement = userName[new Random().nextInt(userName.length)];
 
-        }
 
+        System.out.println(randomElement);
 
     }
 }
