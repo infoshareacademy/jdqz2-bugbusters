2514dc39155b16ba1417b6e344673430feb5096c
Dorota Dembowska <dorotadembowska@gmail.com>
2018-08-26 20:47:49 +0200

    dodalam dane

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,46 @@
+import java.util.Random;
+
 public class Main {
-    public void setUser(User user) {
+
+    public void registration(User user) {
         this.user = user;
+
+    String userName [] = {"Karol", "Dorota", "Wojtek", "Marian", "Jan"};
+    String randomuserName = userName[new Random().nextInt(userName.length)];
+        System.out.println(randomuserName);
+    String firstName [] = {"Karol", "Dorota", "Wojtek", "Marian", "Jan"};
+    String randomfirstName = firstName[new Random().nextInt(firstName.length)];
+        System.out.println(randomfirstName);
+    String lastName [] = {"Kowalski", "Nowak", "Turecki", "Jankowski", "Sobieski"};
+    String randomlastName = lastName[new Random().nextInt(lastName.length)];
+    System.out.println(randomlastName);
+
+
+
     }
+
 }
 
-        ////////
-        User user = new User();
-        user.setName(randomUserName);
-        user.setMail();
-        user.setLogin();
-
-        return user;
-        /////////////
-
-        preapreUsers(Integer numerOfUsers) {
-            List<User> users;
-            for (int < 0; i < numberOfUsers) {
-                users.add(generateRandomUser());
-            }
-
-            return users;
-        }
-
-        class Obj {
-            List<User>user;
-
-
+        /// User user = new User();
+//        user.setName(randomUserName);
+//        user.setMail();
+//        user.setLogin();
+//
+//        return user;
+//        /////////////
+//
+//        preapreUsers(Integer numerOfUsers) {
+//            List<User> users;
+//            for (int < 0; i < numberOfUsers) {
+//                users.add(generateRandomUser());
+//            }
+//
+//            return users;
+//        }
+//
+//        class Obj {
+//            List<User>user;
+       /
 
     }
 }
