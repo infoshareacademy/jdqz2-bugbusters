b5e28c6e21396ff734a69c117e761a6b70600d9f
Karol Marczyk <karmarcz@gmail.com>
2018-09-16 19:41:05 +0200

    Made corrections from githuba comments

--- src/main/java/com/infoshare/bug_busters/payment/ListPayment.java
+++ src/main/java/com/infoshare/bug_busters/payment/ListPayment.java
@@ -1,39 +1,24 @@
 package main.java.com.infoshare.bug_busters.payment;
 
-
-import com.infoshare.bug_busters.random.RandomDataGenerator;
-
 public class ListPayment {
     //Troche skjopiowane ale zatwierdzenie typu danych dla preprarePayments
     private String cardNumber;
     private String expires;
-    private String CCV;
+    private String ccv;
     //Jak się mają zapisywać dane
     public ListPayment(String cardNumber, String expires, String CCV){
         this.cardNumber=cardNumber;
         this.expires=expires;
-        this.CCV=CCV;
+        this.ccv=CCV;
     }
 
-    //Główna metoda zwracająca losowe dane
-    public static ListPayment preparePayments() {
-
-        //najprawdopodobnie niepotrzebne boolean ale zostawiam
-        String CardRandomString = RandomDataGenerator.CardNumberRandom();
-        String CardExpiresString = RandomDataGenerator.Expires();
-        String randomCCVString = RandomDataGenerator.RandomCCV();
-
-        //Co i jak ma zwracać
-        return new ListPayment(CardRandomString,CardExpiresString,randomCCVString);
-
-    }
     //zamienienie Hashcodu na coś czytelnego
     @Override
     public String toString() {
         return "ListPayment{" +
                 "cardNumber='" + cardNumber + '\'' +
                 ", expires='" + expires + '\'' +
-                ", CCV='" + CCV + '\'' +
+                ", CCV='" + ccv + '\'' +
                 '}';
     }
 }
--- src/main/java/com/infoshare/bug_busters/payment/PaymentGeneratorApp.java
+++ src/main/java/com/infoshare/bug_busters/payment/PaymentGeneratorApp.java
@@ -1,30 +1,40 @@
 package main.java.com.infoshare.bug_busters.payment;
 
+import com.infoshare.bug_busters.random.RandomDataGenerator;
 import main.java.com.infoshare.bug_busters.payment.ListPayment;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PaymentGeneratorApp {
-        public static void main(String[] args) {
 
-            //Stworzenie Skaneru do pobrania ile danych ma być wyplutych
-        Integer iloscDanych=10;
+    //Główna metoda zwracająca losowe dane
+    public static ListPayment preparePayments() {
 
+        //najprawdopodobnie niepotrzebne boolean ale zostawiam
+        String CardRandomString = RandomDataGenerator.CardNumberRandom();
+        String CardExpiresString = RandomDataGenerator.Expires();
+        String randomCCVString = RandomDataGenerator.RandomCCV();
+
+        //Co i jak ma zwracać
+        return new ListPayment(CardRandomString, CardExpiresString, randomCCVString);
+
+    }
+
+    public static void safeToCollection() {
+
+        //Stworzenie Skaneru do pobrania ile danych ma być wyplutych
+        Integer iloscDanych = 10;
 
         //Kolekcja która przetrzymuje te dane
         List<ListPayment> paymentsData = new ArrayList<>();
 
         //pentla w której wykorzystujemy metode z ListPayment
-        for(int i=1;i<=iloscDanych;i++){
+        for (int i = 1; i <= iloscDanych; i++) {
 
-            paymentsData.add(ListPayment.preparePayments());
-            System.out.println(ListPayment.preparePayments());
-
-            System.out.println("..............................");
+            paymentsData.add(preparePayments());
 
         }
-
 
     }
 
