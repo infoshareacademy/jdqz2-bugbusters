7c23afc4aa23bd6ec0a91e7d510eba77309aac63
Karol Warakomski <k.warakomski1@gmail.com>
2018-10-08 10:35:40 +0200

    code refactor

--- src/main/java/com/infoshare/bug_busters/utils/Waits.java
+++ src/main/java/com/infoshare/bug_busters/utils/Waits.java
@@ -15,10 +15,6 @@ import java.util.concurrent.TimeUnit;
 public class Waits {
 
     private static final int EXPLICIT_WAIT_TIMEOUT = 10;
-    private static final int PULLING_TIMEOUT_IN_SEC = 3;
-    private static final int DEFAULT_TIMEOUT_IN_SEC = 15;
-
-
 
     private WebDriver driver;
 
@@ -31,19 +27,13 @@ public class Waits {
         wait.until(ExpectedConditions.visibilityOf(element));
     }
 
-    public void waitForElementToNotBeVisible(WebElement element) {
-        WebDriverWait wait = new WebDriverWait(driver, EXPLICIT_WAIT_TIMEOUT);
-        wait.until(ExpectedConditions.invisibilityOf(element));
-    }
-
     public void waitForElementToBeClickable(WebElement element){
         WebDriverWait wait = new WebDriverWait(driver, EXPLICIT_WAIT_TIMEOUT);
         wait.until(ExpectedConditions.elementToBeClickable(element));
     }
 
-    public void waitForElementToBeSOMETHING(By by) {
+    public void waitForTextInElementToBePresent(By by) {
         WebDriverWait wait = new WebDriverWait(driver, EXPLICIT_WAIT_TIMEOUT);
         wait.until(ExpectedConditions.textToBePresentInElement(by, "1 item(s) in cart"));
     }
-
 }
