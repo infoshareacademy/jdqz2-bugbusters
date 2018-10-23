575b55f553578aa6b88ea5d0ad5e71ce903b7582
dorotadembowska <dorotadembowska@gmail.com>
2018-10-23 12:44:15 -0400

    xml methods

--- src/main/java/com/infoshare/bug_busters/registration/UserDataGenerator.java
+++ src/main/java/com/infoshare/bug_busters/registration/UserDataGenerator.java
@@ -1,6 +1,12 @@
 package com.infoshare.bug_busters.registration;
+import com.infoshare.bug_busters.TestData;
+import com.infoshare.bug_busters.payment.PaymentData;
 import com.infoshare.bug_busters.random.RandomDataGenerator;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -35,6 +41,13 @@ public class UserDataGenerator {
         }
 
         return userData;
+    }
+    public List<UserData> getDataFromXml() throws JAXBException {
+        JAXBContext jc = JAXBContext.newInstance(TestData.class);
+
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        TestData testData = (TestData) unmarshaller.unmarshal(new File("/jdqz2-bugbusters/TestData.xml"));
+        return testData.getUserData();
     }
 
 }
--- src/main/java/com/infoshare/bug_busters/shipping/ShippingDataGenerator.java
+++ src/main/java/com/infoshare/bug_busters/shipping/ShippingDataGenerator.java
@@ -1,9 +1,16 @@
 package com.infoshare.bug_busters.shipping;
 
 
+import com.infoshare.bug_busters.TestData;
+import com.infoshare.bug_busters.payment.PaymentData;
 import com.infoshare.bug_busters.random.RandomDataGenerator;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class ShippingDataGenerator {
 
@@ -16,6 +23,13 @@ public class ShippingDataGenerator {
         String country = RandomDataGenerator.prepareCountry();
 
         return new ShippingData(houseNumber, streetName, city, postCode, country);
+    }
+    public List<ShippingData> getDataFromXml() throws JAXBException {
+        JAXBContext jc = JAXBContext.newInstance(TestData.class);
+
+        Unmarshaller unmarshaller = jc.createUnmarshaller();
+        TestData testData = (TestData) unmarshaller.unmarshal(new File("/jdqz2-bugbusters/TestData.xml"));
+        return testData.getShippingData();
     }
 }
 
