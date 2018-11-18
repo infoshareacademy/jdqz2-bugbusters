cf91620d614d1d70475c5c29e109de40f5e4b5ba
karolwarak <k.warakomski1@gmail.com>
2018-11-18 20:49:31 +0100

    create json class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,27 @@
 
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.infoshare.bug_busters.pageObject.HomePage;
 import com.infoshare.bug_busters.payment.PaymentDataGenerator;
 import com.infoshare.bug_busters.random.RandomDataGenerator;
+import com.infoshare.bug_busters.registration.UserData;
 import com.infoshare.bug_busters.registration.UserDataGenerator;
 import com.infoshare.bug_busters.shipping.ShippingDataGenerator;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        UserDataGenerator userDataGenerator = new UserDataGenerator(new RandomDataGenerator());
+        /*UserDataGenerator userDataGenerator = new UserDataGenerator(new RandomDataGenerator());
 
         ShippingDataGenerator shippingDataGenerator = new ShippingDataGenerator(new RandomDataGenerator());
 
-        PaymentDataGenerator paymentDataGenerator = new PaymentDataGenerator();
+        PaymentDataGenerator paymentDataGenerator = new PaymentDataGenerator(new RandomDataGenerator());
 
         //do prezentacji : listy obiektow, uzytkownik decyduje ile obiektow chce otrzymac
 
@@ -24,6 +29,9 @@ public class Main {
 
         System.out.println(userDataGenerator.prepareUserData());
 
-        System.out.println(shippingDataGenerator.createListWithObjectsWithRandomChoiceDate(1 ));
+        System.out.println(shippingDataGenerator.createListWithObjectsWithRandomChoiceDate(1 ));*/
+
+        // https://www.baeldung.com/jackson-object-mapper-tutorial
+
     }
 }
---
+++ src/main/java/com/infoshare/bug_busters/dataFromJson/ConverClassJson.java
@@ -0,0 +1,10 @@
+package com.infoshare.bug_busters.dataFromJson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverClassJson {
+
+    ObjectMapper mapper = new ObjectMapper();
+
+
+}
